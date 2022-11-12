e336dd7c35c5d687032e2671a3579a88cd3c9f8c
l1roii <71549088+l1roii@users.noreply.github.com>
2022-11-12 01:47:41 +0100

    about me

---
+++ about_me.php
@@ -0,0 +1,48 @@
+<?php  include("Includes/header.php")?>
+
+<div class="box">
+    <div class="container">
+        <div class="column-about">
+            <div class="column-child">
+                <img width="300px" height="300px" src="Assets/Images/home_image.jpg">
+            </div>
+            <div class="column-child">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0"
+                         aria-valuemax="100"></div>
+                </div>
+                <span>CSS3</span> <span>75%</span>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0"
+                         aria-valuemax="100"></div>
+                </div>
+                <span>PHP</span> <span>50%</span>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                         aria-valuemax="100"></div>
+                </div>
+                <span>JAVASCRIPT</span> <span>50%</span>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="90" aria-valuemin="0"
+                         aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+        <div class="column-about">
+            <div class="column-child">
+                <p><span >Name: </span> <span>Lirianë Berisha</span></p>
+                <p><span >Email: </span> <span>lira45@hotmail.com</span></p>
+                <p><span >Phone: </span> <span>+383 49 354 502</span></p>
+            </div>
+            <div class="column-child">
+                    <h5> About me</h5>
+                <p class="lead">
+                    Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id
+                    imperdiet et, porttitor
+                    at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla
+                    porttitor accumsan tincidunt.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>

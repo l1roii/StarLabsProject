54b17d167da799c2909e29f216db1bc86c0e8b1f
l1roii <71549088+l1roii@users.noreply.github.com>
2022-11-13 20:55:08 +0100

    created projects page

--- projects.php
+++ projects.php
@@ -1,80 +1,51 @@
 <?php  include("Includes/header.php")?>
-<body class="container particle">
-    <!--<div class="content">-->
-    <div id="large-header" class="large-header">
-        <canvas id="demo-canvas"></canvas>
-        <!--<h1 class="main-title">Connect <span class="thin">Three</span></h1>-->
-    </div>
-    <!--</div> /content -->
-    <!-- /container -->
 
-<!-- Slideshow container -->
-<!--<div class="slideshow-container">-->
-<!---->
-<!--    <div class="mySlides fade">-->
-<!--        <div class="courses-container">-->
-<!--            <div class="course">-->
-<!--                <div class="course-preview">-->
-<!--                    <h6>Project name:</h6>-->
-<!--                    <h2>Camera Shop</h2>-->
-<!--                    <a href="#">View all details <i class="fas fa-chevron-right"></i></a>-->
-<!--                </div>-->
-<!--                <div class="course-info">-->
-<!--                    <h6>Technology used</h6>-->
-<!--                    <h2>HTML, CSS, Java Script, PHP, MySQL</h2>-->
-<!--                    <button class="btn">Continue</button>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--    <div class="mySlides fade">-->
-<!--        <div class="courses-container">-->
-<!--            <div class="course">-->
-<!--                <div class="course-preview">-->
-<!--                    <h6>Project name:</h6>-->
-<!--                    <h2>Healthcare Mangment System</h2>-->
-<!--                    <a href="#">View all details <i class="fas fa-chevron-right"></i></a>-->
-<!--                </div>-->
-<!--                <div class="course-info">-->
-<!--                    <h6>Technology used</h6>-->
-<!--                    <h2>HTML, CSS, Java Script, PHP, MySQL</h2>-->
-<!--                    <button class="btn">Continue</button>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--    <div class="mySlides fade">-->
-<!--        <div class="courses-container">-->
-<!--            <div class="course">-->
-<!--                <div class="course-preview">-->
-<!--                    <h6>Project name:</h6>-->
-<!--                    <h2>Cyber Security Info</h2>-->
-<!--                    <a href="#">View all details <i class="fas fa-chevron-right"></i></a>-->
-<!--                </div>-->
-<!--                <div class="course-info">-->
-<!--                    <h6>Technology used</h6>-->
-<!--                    <h2>HTML, CSS, Java Script, PHP, MySQL</h2>-->
-<!--                    <button class="btn">Continue</button>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-    <!-- Next and previous buttons -->
-<!--    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>-->
-<!--    <a class="next" onclick="plusSlides(1)">&#10095;</a>-->
-<!--</div>-->
-<!--<br>-->
-<!---->
-<!-- The dots/circles -->
-<!--<div style="text-align:center">-->
-<!--    <span class="dot" onclick="currentSlide(1)"></span>-->
-<!--    <span class="dot" onclick="currentSlide(2)"></span>-->
-<!--    <span class="dot" onclick="currentSlide(3)"></span>-->
-<!--</div>-->
-    <script src="Assets/Js/script.js"></script>
+<body class="container particle">
+<div id="large-header" class="large-header">
+    <canvas id="demo-canvas"></canvas>
+    <div class="container_project main-title">
+        <div class="column_project">
+            <div class="column-child">
+                <div class="box_project">
+                    <div class="image">
+                        <img  src="Assets/Images/Cyber_security.png">
+                    </div>
+                    <div class="description">
+                        <p>A website for renting and selling cameras - Project for Web Development Training</p>
+                        <p>To view more about this click <a href="">here</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="column-child">
+                <div class="box_project">
+                    <div class="image">
+                        <img  src="Assets/Images/Camera_shop.png">
+                    </div>
+                    <div class="description">
+                        <p>A website for information in cyber security - Project for University</p>
+                        <p>To view more about this click <a href="">here</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="column_project">
+            <div class="column-child">
+                <div class="box_project">
+                    <div class="image">
+                        <img  src="Assets/Images/Camera_shop.png">
+                    </div>
+                    <div class="description">
+                        <p>A website for information in cyber security - Project for University</p>
+                        <p>To view more about this click <a href="">here</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script src="Assets/Js/script.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenLite.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/easing/EasePack.min.js"></script>
 <script src="Assets/Js/constellation.js"></script>
